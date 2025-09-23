e8ca8d9687c03b97341cf7d7b78ecdc3e101203d
Enzo Bettini <enzobettini@hotmail.com>
2025-09-23 18:21:32 -0300

    V0 (Alteração exemplo Service)

---
+++ prontodog-banho/src/main/java/backend/prontodogbanho/service/ClienteService.java
@@ -0,0 +1,33 @@
+package backend.prontodogbanho.service;
+
+import backend.prontodogbanho.model.Cliente;
+import backend.prontodogbanho.repository.ClienteRespository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+    private final ClienteRespository clienteRespository;
+
+    public ClienteService(ClienteRespository clienteRespository) {
+        this.clienteRespository = clienteRespository;
+    }
+
+    public List<Cliente> listarTodos() {
+        return clienteRespository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRespository.findById(id);
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return clienteRespository.save(cliente);
+    }
+
+    public void deletar(Long id) {
+        clienteRespository.deleteById(id);
+    }
+}
